2482f09a16d72c28e93e64e434140b97e5944630
Otto Hermansson <otto.hermansson@notitium.se>
2017-04-18 00:44:04 +0200

    Yeay

--- LaserGnome/Form1.Designer.cs
+++ LaserGnome/Form1.Designer.cs
@@ -67,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 186);
+            this.ClientSize = new System.Drawing.Size(160, 168);
             this.Controls.Add(this.isConnectedToLaserOS);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
--- LaserGnome/Form1.cs
+++ LaserGnome/Form1.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Lasergnome
 {
@@ -12,7 +18,9 @@ namespace Lasergnome
         Arduinome arduinome;
         Timer updateStateTimer = new Timer();
         int currentEffect = 0;
+        int currentPattern = 0;
         IntPtr laserOSWindow = IntPtr.Zero;
+        IEnumerable<XElement> buttonMapping;
 
         public Form1()
         {
@@ -27,6 +35,9 @@ namespace Lasergnome
             updateStateTimer.Interval = 250;
             updateStateTimer.Enabled = true;
             updateStateTimer.Start();
+
+            XDocument data = XDocument.Load(Application.StartupPath + "\\resources\\settings.xml");
+            buttonMapping = data.Descendants("button");      
         }
 
         private void ButtonUp(object sender, ButtonEventArgs e)
@@ -64,19 +75,45 @@ namespace Lasergnome
             if (y == 7) {
                 SendEffectKey(x);
             }
+
+            if (y < 7) {
+                int buttonNumber = (y * 8) + x +1;
+                if (buttonMapping.Any(a => a.Attribute("id").Value == buttonNumber.ToString()))
+                {
+
+                    XElement button = buttonMapping.First(a => a.Attribute("id").Value == buttonNumber.ToString());
+                    SendKey(button.Descendants("pattern").First().Attribute("key").Value);
+
+                    int oldy = currentPattern / 8;
+                    int oldx = currentPattern - (oldy * 8);
+                    arduinome.setLed((byte)oldx, (byte)oldy, false);
+                    currentPattern = buttonNumber - 1;
+
+                    if (button.Descendants("effect").Count() == 1)
+                    {
+                        string effect = button.Descendants("effect").First().Attribute("id").Value;
+                        SendEffectKey(int.Parse(effect));
+                    }
+                }
+            }
+        }
+
+        private void SendKey(string data)
+        {
+            if (laserOSWindow != IntPtr.Zero)
+            {
+                IntPtr currentWindow = GetForegroundWindow();
+                SetForegroundWindow(laserOSWindow);
+                SendKeys.Send(data);
+                SetForegroundWindow(currentWindow);
+            }
         }
 
         private void SendEffectKey(int number)
         {
             if (number > -1 && number < 8)
             {
-                if (laserOSWindow != IntPtr.Zero)
-                {
-                    IntPtr currentWindow = GetForegroundWindow();
-                    SetForegroundWindow(laserOSWindow);
-                    SendKeys.Send(number.ToString());
-                    SetForegroundWindow(currentWindow);
-                }
+                SendKey(number.ToString());
                 currentEffect = number;
                 arduinome.setRow(7, false, false, false, false, false, false, false, false);
             }
@@ -95,6 +132,11 @@ namespace Lasergnome
         {
             // Toggle selected effect led.
             arduinome.setLed((byte)currentEffect, 7, !arduinome.GetLedState(currentEffect, 7));
+
+            int oldy = currentPattern / 8;
+            int oldx = currentPattern - (oldy * 8);
+
+            arduinome.setLed((byte)oldx, (byte)oldy, !arduinome.GetLedState(oldx, oldy));
 
             #region Check for laserOS application.
             laserOSWindow = IntPtr.Zero;
